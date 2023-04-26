eccd98cd492dea555b357edb91897bcde70d7091
michael <micha3l_255@yahoo.com>
2023-04-26 02:23:23 -0500

    csv

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
 
 
-# Press the green button in the gutter to run the script.
+def learning_csv():
+    data = open("resources/files/example.csv", encoding="utf-8")
+    csv_data = csv.reader(data)
+    data_lines = list(csv_data)
+    for i in data_lines[:5]:
+        print(i)
+    # using list
+    emails = list(a[3] for a in data_lines[1:5])
+    print(emails)
+    # using map
+    names = map(lambda x: x[2], data_lines[1:5])
+    print(list(names))
+
+    # WRITING A CSV FILE
+    file_to_output = open("resources/files/saving-example.csv", mode="w", newline="")
+    csv_writer = csv.writer(file_to_output, delimiter=",")
+    csv_writer.writerow(["a", "b", "c"])
+    csv_writer.writerows([["1", "2", "3"], ["4", "5", "6"]])
+    file_to_output.close()
+
+    # APPENDING A CSV FILE
+    file_to_output = open("resources/files/saving-example.csv", mode="a", newline="")
+    csv_writer = csv.writer(file_to_output, delimiter=",")
+    csv_writer.writerow(["x", "y", "z"])
+    file_to_output.close()
+
+    data = open("resources/files/saving-example.csv", encoding="utf-8")
+    csv_data = csv.reader(data)
+    data_lines = list(csv_data)
+    print(data_lines)
+
+
+def learning_panda():
+    # Replace "filename.csv" with the name of your CSV file
+    df = pd.read_csv("resources/files/example.csv")
+    # Set the options to display all rows and columns
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    # Print the first 5 rows of the DataFrame
+
+    print(df.head())
+    print(df.tail())
+
+    print("Dataframe to list")
+    dataframe_list = df.values.tolist()
+
+    for index, row in df.head(5).iterrows():
+        for key, value in row.items():
+            print(f"{key}: {value}")
+    emails = list(df.head(5)["email"])
+    print(emails)
+
+    # Create a DataFrame
+    df = pd.DataFrame({'Name': ['Alice', 'Bob', 'Charlie'], 'Age': [25, 30, 35]})
+
+    # Write the DataFrame to a CSV file
+    df.to_csv("resources/files/panda-saving-example.csv", index=False)
+
+
+def divider(title: str):
+    print(f"==========={title.upper()}===========")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    divider("learning csv")
+    learning_csv()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    divider("learning panda")
+    learning_panda()
